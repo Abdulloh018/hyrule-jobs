4339ad352b5308fcb3b5935757f9c2155f09a865
Abdulloh <abdullohnosirov017@gmail.com>
2023-09-21 16:12:04 +0500

    changed <script> to <script setup>

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <header>
       <div class="order">
         <div class="title">
-          <img src="./assets/heart.svg" alt="site logo">
+          <img src="./assets/heart.svg" alt="site logo" />
           <h1>Hyrule Jobs</h1>
         </div>
         <button @click="handleClick('title')">order by title</button>
@@ -11,78 +11,69 @@
         <button @click="handleClick('location')">order by location</button>
       </div>
     </header>
-    <JobList :jobs="jobs" :order="order"/>
+    <JobsList :jobs="jobs" :order="order" />
   </div>
-
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from "vue";
-import JobList from './components/JobsList.vue'
-import Job from './types/Job'
-import OrderTerm from './types/OrderTerm'
+<script lang="ts" setup>
+import { reactive, ref, toRefs } from "vue";
+import JobsList from "./components/JobsList.vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
-export default defineComponent({
-  name: "App",
-  components: { JobList },
-  setup() {
-    // const state = reactive({
-    //   name: 'Link',
-    //   age: 25 as string | number
-    // })
+// const state = reactive({
+//   name: 'Link',
+//   age: 25 as string | number
+// })
 
-    // return { ...toRefs(state) }
+// return { ...toRefs(state) }
 
-    // const name = ref('Link')
-    // const age = ref<number | string>(25)
+// const name = ref('Link')
+// const age = ref<number | string>(25)
 
-    // return {name , age}
-    
-    const jobs = ref<Job[]>([
-      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
-      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
-      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
-      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
-    ])
+// return {name , age}
 
-    const order = ref<OrderTerm>('title')
+const jobs = ref<Job[]>([
+  { title: "farm worker", location: "lon lon ranch", salary: 30000, id: "1" },
+  { title: "quarryman", location: "death mountain", salary: 40000, id: "2" },
+  { title: "flute player", location: "the lost woods", salary: 35000, id: "3" },
+  { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+  { title: "prison guard", location: "gerudo valley", salary: 32000, id: "5" },
+]);
 
-    const handleClick = (term: OrderTerm) => {
-      order.value = term
-    }
+const order = ref<OrderTerm>("title");
 
-    return { jobs, handleClick, order }
-  }
-});
+const handleClick = (term: OrderTerm) => {
+  order.value = term;
+};
 </script>
 
 <style>
-  header {
-    text-align: center;
-  }
-  header .order {
-    margin-top: 20px;
-  }
-  button {
-    margin: 0 10px;
-    color: #1195c9;
-    border: 3px solid #1195c9;
-    background: #d5f0ff;
-    padding: 8px 16px;
-    border-radius: 4px;
-    cursor: pointer;
-    font-weight: bold;
-  }
-  header .title{
-    display: flex;
-    justify-content: center;
-  }
-  header img {
-    width: 60px;
-    margin-right: 20px;
-  }
-  header h1 {
-    font-size: 3em;
-  }
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
+}
 </style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="job-list">
-    <p>Ordered by {{ order }}</p>
+    <div class="search-jobs">
+      <input
+        type="text"
+        v-model="search"
+        placeholder="Search"
+        class="search-input"
+      />
+    </div>
     <transition-group name="list" tag="ul">
       <li v-for="job in orderedJobs" :key="job.id">
-        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <h2>{{ job.title }} <span class="in">in</span> {{ job.location }}</h2>
         <div class="salary">
-          <img src="../assets/rupee.svg" alt="rupee icon">
+          <img src="../assets/rupee.svg" alt="rupee icon" />
           <p>{{ job.salary }} rupees</p>
         </div>
         <div class="description">
@@ -21,65 +28,76 @@
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType } from "vue";
+<script lang="ts" setup>
+import { computed, ref, defineProps } from "vue";
 import Job from "../types/Job";
 import OrderTerm from "../types/OrderTerm";
 
-export default defineComponent({
-  props: {
-    jobs: {
-      required: true,
-      type: Array as PropType<Job[]>,
-    },
-    order: {
-      required: true,
-      type: String as PropType<OrderTerm>
-    }
-  },
-  setup(props) {
-    const orderedJobs = computed(() => {
-      return [...props.jobs].sort((a: Job, b: Job) => {
-        return a[props.order] > b[props.order] ? 1 : -1
-      })
-    })
+interface Props {
+  jobs: Job[],
+  order: OrderTerm
+}
 
-    return { orderedJobs } 
+const props = defineProps<Props>();
+
+const search = ref("");
+
+const orderedJobs = computed(() => {
+  
+  let sortedJobs: Job[] = [...props.jobs]
+
+  if(search.value !== '') {
+    sortedJobs = sortedJobs.filter(item => item.title.toLowerCase().includes(search.value.toLowerCase()))
   }
+  
+  return sortedJobs.sort((a: Job, b: Job) => a[props.order] > b[props.order] ? 1 : -1);
 });
 </script>
 
 <style scoped>
- .job-list {
-    max-width: 960px;
-    margin: 40px auto;
-  }
-  .job-list ul {
-    padding: 0;
-  }
-  .job-list li {
-    list-style-type: none;
-    background: white;
-    padding: 16px;
-    margin: 16px 0;
-    border-radius: 4px;
-  }
-  .job-list h2 {
-    margin: 0 0 10px;
-    text-transform: capitalize;
-  }
-  .salary {
-    display: flex;
-  }
-  .salary img {
-    width: 30px;
-  }
-  .salary p {
-    color: #17bf66;
-    font-weight: bold;
-    margin: 10px 4px;
-  }
-  .list-move {
-    transition: all 0.7s;
-  }
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.in {
+  color: rgb(0, 166, 255);
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+.list-move {
+  transition: all 0.7s;
+}
+
+.search-jobs {
+  display: flex;
+  justify-content: center;
+}
+
+.search-input {
+  padding: 10px;
+  width: 50%;
+}
 </style>
